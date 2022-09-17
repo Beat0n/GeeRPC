ef2dd653cbb0fc6fc3745b1bf6d6fc6032b9fd92
Beat0n <hujizheuestc@163.com>
2022-09-17 23:31:05 +0800

    ""

---
+++ main/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"GeeRPC"
+	"GeeRPC/codec"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+func startServer(addr chan string) {
+	l, err := net.Listen("tcp", "127.0.0.1:8000")
+	if err != nil {
+		log.Fatal("network error:", err)
+	}
+	log.Println("start rpc server on", l.Addr())
+	addr <- l.Addr().String()
+	GeeRPC.Accept(l)
+}
+
+func main() {
+	addr := make(chan string)
+	go startServer(addr)
+
+	conn, _ := net.Dial("tcp", <-addr)
+	defer conn.Close()
+
+	time.Sleep(1e9)
+	json.NewEncoder(conn).Encode(GeeRPC.DefaultOption)
+	cc := codec.NewGobCodec(conn)
+	// send Request && receive Response
+	for i := 0; i < 5; i++ {
+		h := &codec.Header{
+			ServiceMethod: "Foo.Sum",
+			Seq:           uint64(i),
+		}
+		cc.Write(h, fmt.Sprintf("geerpc req %d", h.Seq))
+		cc.ReadHeader(h)
+		var reply string
+		cc.ReadBody(&reply)
+		log.Println("reply:", reply)
+	}
+}
